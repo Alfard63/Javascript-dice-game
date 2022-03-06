a1d75ce9bebedc5e78aad665c8dd8c4e76a96c8e
Alfard63 <alfard.dev@gmail.com>
2022-03-06 16:00:12 +0100

    UPDATE: add a modal view to see the game rules

--- JS/classes/view.js
+++ JS/classes/view.js
@@ -1,17 +1,17 @@
-
-
 export default class View {
     constructor() {        
         this.player1CurrentScore = document.getElementById('player1CurrentScore')
         this.player2CurrentScore = document.getElementById('player2CurrentScore')
         this.player1GlobalScore = document.getElementById('player1GlobalScore')
         this.player2GlobalScore = document.getElementById('player2GlobalScore')
-        this.volumeBtn = document.getElementById('volumeBtn')        
+        this.volumeBtn = document.getElementById('volumeBtn') 
+        this.helpBtn = document.getElementById('helpBtn')       
         this.label1 = document.getElementById('label1')
         this.label2 = document.getElementById('label2')
         this.board = document.getElementById('board')
         this.dice = document.getElementById('dice')          
     }
+
 /* Update the view of Current Score and Global Score for each player */
     updateScoresView(player1, player2) {
         this.player1CurrentScore.innerHTML = player1.currentScore
@@ -19,6 +19,7 @@ export default class View {
         this.player1GlobalScore.innerHTML = player1.globalScore
         this.player2GlobalScore.innerHTML = player2.globalScore
     }
+
 /* Update the view of the dice */
     updateDiceview(int) {
         switch (int) {
@@ -43,29 +44,58 @@ export default class View {
                 break;
         }
     }
+
 /* Change the colors of the background, the dot and the volume button when the turn change */
     updateTurnView(player1Turn) {
 
         if (player1Turn) {
             this.board.classList.add('player1-turn')
             this.board.classList.remove('player2-turn')
-            this.label1.style.setProperty('--color-1', '#dc143c')
-            this.label2.style.setProperty('--color-2', '#f8f9fa')
-            this.volumeBtn.style.setProperty('--color-3', '#ebe8e8')
+            this.label1.style.setProperty('--label-color-1', '#dc143c')
+            this.label2.style.setProperty('--label-color-2', '#f8f9fa')
+            this.volumeBtn.style.setProperty('--btn-color', '#f8f9fa')
+            this.helpBtn.style.setProperty('--btn-color', '#f8f9fa')
         } 
         else {
             this.board.classList.add('player2-turn')
             this.board.classList.remove('player1-turn')
-            this.label2.style.setProperty('--color-2', '#dc143c')
-            this.label1.style.setProperty('--color-1', '#f8f9fa')
-            this.volumeBtn.style.setProperty('--color-3', '#f8f9fa')
+            this.label2.style.setProperty('--label-color-2', '#dc143c')
+            this.label1.style.setProperty('--label-color-1', '#f8f9fa')
+            this.volumeBtn.style.setProperty('--btn-color', '#d8d8d8')
+            this.helpBtn.style.setProperty('--btn-color', '#d8d8d8')
         }
     }
+/* Change the symbol of the volume button when is clicked */
+    updateVolumeView(songsMuted) {
+        !songsMuted ? this.volumeBtn.innerHTML = '<i class="bi bi-volume-up text-dark"></i>' : this.volumeBtn.innerHTML = '<i class="bi bi-volume-mute"></i>'
+    }
+
+/* add an effect when the button is clicked */
+    btnClickEffect(element, event) {
+        if (event.type === 'mouseup' || event.type === 'mouseout' ) {
+            element.classList.add('shadow')
+            element.classList.replace('fs-3', 'fs-2')
+        }
+        if (event.type === 'mousedown'){
+            element.classList.remove('shadow')
+            element.classList.replace('fs-2', 'fs-3')
+        }
+        event.stopPropagation()
+    }
+
 /* Change the colors of the background and lock buttons when a player win */
     victory(player, rollDiceBtn, holdBtn) {
         this.board.classList.remove('player1-turn')
         this.board.classList.remove('player2-turn')
         this.board.classList.add(`${player}-victory`)
+        if (player === 'player1') {
+            this.label1.style.setProperty('--label-color-1', '--victory-color')
+            this.label2.style.setProperty('--label-color-2', '--loose-color')
+        } else {
+            this.label1.style.setProperty('--label-color-1', '--loose-color')
+            this.label2.style.setProperty('--label-color-2', '--victory-color')
+        }
+
         rollDiceBtn.classList.add(`disabled`)
         holdBtn.classList.add(`disabled`)
     }
--- JS/script.js
+++ JS/script.js
@@ -3,7 +3,7 @@
 /* View manage all view changes                                     /
 /* Player manage the players scores and actions                     /
 /* Dice return a random value between 1 and 6                       /
-/* Song manage song and play it if mute button is not activate    /
+/* Song manage song and play it if mute button is not activate      /
  *******************************************************************/
 import View from './classes/view.js'
 import Player from './classes/player.js'
@@ -12,31 +12,36 @@ import Song from './classes/song.js'
 
 document.addEventListener("DOMContentLoaded", () => {
 
-/* Instanciating all buttons and the board of the game */   
+    /* Instanciating all buttons and the board of the game */
     const newGameBtn = document.getElementById('newGameBtn')
     const rollDiceBtn = document.getElementById('rollDiceBtn')
     const holdBtn = document.getElementById('holdBtn')
     const volumeBtn = document.getElementById('volumeBtn')
+    const helpBtn = document.getElementById('helpBtn')
 
     const board = document.getElementById('board')
 
-/* Create all game songs */
-    const clickSound = new Song ('../songs/click.mp3')
-    const wrongSound = new Song ('../songs/wrong.mp3')
-    const winSound = new Song ('../songs/win.mp3')
-    const holdSound = new Song ('../songs/correct.mp3')
+    /* Create all game songs */
+    const clickSound = new Song('../songs/click.mp3')
+    const wrongSound = new Song('../songs/wrong.mp3')
+    const winSound = new Song('../songs/win.mp3')
+    const holdSound = new Song('../songs/correct.mp3')
     const rollgSound = new Song('../songs/dice-roll.mp3')
 
-/* Create a new View, 2 Player and Dice */
+    /* Create a new View, 2 Player and Dice */
     const view = new View
-    const player1 = new Player (0, 0)
-    const player2 = new Player (0, 0)
+    const player1 = new Player()
+    const player2 = new Player()
     const dice = new Dice
-/* Initialising some variables */
+    const myModal = new bootstrap.Modal(document.getElementById('myModal'))
+
+    /* Initialising some variables */
     let player1Turn = true
     let songsMuted = false
 
-    function newGame() {
+    /* Function to start a new game */
+    const newGame = () => {
+        player1Turn = true
         player1.initPlayer(0, 0)
         player2.initPlayer(0, 0)
         view.updateScoresView(player1, player2)
@@ -48,38 +53,53 @@ document.addEventListener("DOMContentLoaded", () => {
             board.classList.remove(`player${i}-victory`)
         }
     }
+    /* function to mute all songs */
+    const muteSound = (event) => {
+        clickSound.playSong(songsMuted)
+        songsMuted = !songsMuted ? true : false
+        view.updateVolumeView(songsMuted)
+        event.stopPropagation()
+    }
 
-   
+
+    /**********************************************************/
+    /*          ADD EVENT CLICK ON NEW GAME BUTTON            */
+    /**********************************************************/
     newGameBtn.addEventListener('click', (event) => {
         clickSound.playSong(songsMuted)
-        player1Turn = true
-        newGame(player1, player2, view, player1Turn)
+        newGame()
         event.stopPropagation()
     })
 
+
+    /**********************************************************/
+    /*          ADD EVENT CLICK ON ROLL DICE BUTTON           */
+    /**********************************************************/
     rollDiceBtn.addEventListener('click', (event) => {
         if (!holdBtn.classList.contains('disabled')) {
             const diceValue = dice.roll()
             rollgSound.playSong(songsMuted)
             view.updateDiceview(diceValue)
-            setTimeout(() => {
-                if (diceValue === 1) {
-                    player1.currentScore = 0
-                    player2.currentScore = 0
-                    wrongSound.playSong(songsMuted)
-                    player1Turn = player1Turn ? false : true
-                }
-                else {
-                    player1Turn ? player1.current(diceValue) : player2.current(diceValue)
-                }
-                view.updateScoresView(player1, player2)
-                view.updateTurnView(player1Turn)
-            }, 500)
+            if (diceValue === 1) {
+                player1.currentScore = 0
+                player2.currentScore = 0
+                wrongSound.playSong(songsMuted)
+                player1Turn = player1Turn ? false : true
+            }
+            else {
+                player1Turn ? player1.current(diceValue) : player2.current(diceValue)
+            }
+            view.updateScoresView(player1, player2)
+            view.updateTurnView(player1Turn)
 
             event.stopPropagation()
         }
     })
 
+
+    /**********************************************************/
+    /*             ADD EVENT CLICK ON HOLD BUTTON             */
+    /**********************************************************/
     holdBtn.addEventListener('click', (event) => {
         if (!holdBtn.classList.contains('disabled')) {
             if (player1Turn) {
@@ -92,39 +112,52 @@ document.addEventListener("DOMContentLoaded", () => {
             view.updateScoresView(player1, player2)
             player1Turn = player1Turn ? false : true
             view.updateTurnView(player1Turn)
-            if (player1.isWin() === true ) {
-                winSound.playSong(songsMuted) 
-                view.victory('player1', rollDiceBtn, holdBtn) 
+            if (player1.isWin() === true) {
+                winSound.playSong(songsMuted)
+                view.victory('player1', rollDiceBtn, holdBtn)
             }
-            if (player2.isWin() === true ) { 
-                winSound.playSong(songsMuted) 
+            if (player2.isWin() === true) {
+                winSound.playSong(songsMuted)
                 view.victory('player2', rollDiceBtn, holdBtn)
             }
             event.stopPropagation()
         }
     })
 
+
+    /**********************************************************/
+    /*         ADD EVENT LISTENERS ON VOLUME BUTTON           */
+    /**********************************************************/
     volumeBtn.addEventListener('click', (event) => {
-        songsMuted ? volumeBtn.innerHTML = '<i class="bi bi-volume-up text-dark"></i>' : volumeBtn.innerHTML = '<i class="bi bi-volume-mute"></i>'
-        songsMuted = !songsMuted ? true : false
-        event.stopPropagation()
+        muteSound(event)
     })
     volumeBtn.addEventListener('mousedown', (event) => {
-        clickSound.playSong(songsMuted)
-        volumeBtn.classList.remove('shadow')
-        volumeBtn.classList.replace('fs-2', 'fs-3')
-        event.stopPropagation()
+        view.btnClickEffect(volumeBtn, event)
     })
     volumeBtn.addEventListener('mouseup', (event) => {
-        volumeBtn.classList.add('shadow')
-        volumeBtn.classList.replace('fs-3', 'fs-2')
-        event.stopPropagation()
+        view.btnClickEffect(volumeBtn, event)
     })
     volumeBtn.addEventListener('mouseout', (event) => {
-        volumeBtn.classList.add('shadow')
-        volumeBtn.classList.replace('fs-3', 'fs-2')
-        event.stopPropagation()
-    }) 
- /* Initialising a new game  */   
+        view.btnClickEffect(volumeBtn, event)
+    })
+
+
+    /**********************************************************/
+    /*           ADD EVENT LISTENERS ON HELP BUTTON           */
+    /**********************************************************/
+    helpBtn.addEventListener('click', (event) => {
+    })
+    helpBtn.addEventListener('mousedown', (event) => {
+        view.btnClickEffect(helpBtn, event)
+    })
+    helpBtn.addEventListener('mouseup', (event) => {
+        view.btnClickEffect(helpBtn, event)
+    })
+    helpBtn.addEventListener('mouseout', (event) => {
+        view.btnClickEffect(helpBtn, event)
+    })
+
+
+    /* Initialising a new game  */
     newGame()
 })
